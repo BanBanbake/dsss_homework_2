34fc17a327d0a87f285a0cec7fb30e14e9ef7d7e
BanBanbake <149976306+BanBanbake@users.noreply.github.com>
2023-11-11 02:22:00 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math-quiz",  # Replace with your package name
+    version="0.1.0",
+    description="A math quiz",
+    author="Mingyang Li",
+    author_email="limingyang0514@gmail.com",
+    url="https://github.com/BanBanbake/dsss_homework_2.gitt",
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here
+        "random",
+        "unittest",
+    ],
+)
